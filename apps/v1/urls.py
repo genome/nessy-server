a06886fb333df6b75340af9fbd8dd0c0071934de
Mark Burnett <mark.m.burnett@gmail.com>
2014-01-15 13:33:07 -0600

    extract common URL pattern

--- apps/v1/urls.py
+++ apps/v1/urls.py
@@ -1,16 +1,17 @@
 from django.conf.urls import patterns, include, url
 from . import views
 
+_RESOURCE_NAME = '(?P<resource_name>\w{1,64})'
 
 urlpatterns = patterns('',
     url(r'^$', views.APIRootView.as_view()),
     url(r'^locks/$', views.LockListView.as_view(), name='lock-list'),
-    url(r'^locks/(?P<resource_name>\w{1,64})/$',
+    url(r'^locks/' + _RESOURCE_NAME + '/$',
         views.LockDetailView.as_view(), name='lock-detail'),
-    url(r'^locks/(?P<resource_name>\w{1,64})/owner/$',
+    url(r'^locks/' + _RESOURCE_NAME + '/owner/$',
         views.OwnerDetailView.as_view(), name='lock-owner'),
-    url(r'^locks/(?P<resource_name>\w{1,64})/requests/$',
+    url(r'^locks/' + _RESOURCE_NAME + '/requests/$',
         views.RequestListView.as_view(), name='lock-requests'),
-    url(r'^locks/(?P<resource_name>\w{1,64})/requests/(?P<pk>\w+)/$',
+    url(r'^locks/' + _RESOURCE_NAME + '/requests/(?P<pk>\w+)/$',
         views.RequestDetailView.as_view(), name='request-detail'),
 )

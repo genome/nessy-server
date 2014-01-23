92e37f16f029c41cc34483b3837810bb65720cca
Mark Burnett <mark.m.burnett@gmail.com>
2014-01-23 09:48:23 -0600

    get claim detail returns 200

--- tests/api/v1/test_claim_detail_get.py
+++ tests/api/v1/test_claim_detail_get.py
@@ -1,12 +1,22 @@
 from ..base import APITest
 
 
-class ClaimDetailGetGeneralSuccessTest(APITest):
-    pass
+POST_URL = '/v1/claims/'
 
-# TODO
-#    def test_should_return_200(self):
-#        pass
+
+class ClaimDetailGetGeneralSuccessTest(APITest):
+    def setUp(self):
+        super(ClaimDetailGetGeneralSuccessTest, self).setUp()
+        self.post_data = {
+            'resource': 'post-resource',
+            'timeout': 0.010,
+        }
+        self.post_response = self.post(POST_URL, self.post_data)
+        self.url = self.post_response.headers['Location']
+        self.response = self.get(self.url)
+
+    def test_should_return_200(self):
+        self.assertEqual(200, self.response.status_code)
 
 # TODO
 #    def test_should_return_creation_time(self):

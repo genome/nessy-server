932b84fcf03fa7d583e632ce80eb6b5858f736b8
Mark Burnett <mark.m.burnett@gmail.com>
2014-01-30 10:36:07 -0600

    remove put support

--- tests/api/v1/test_claim_detail_update.py
+++ tests/api/v1/test_claim_detail_update.py
@@ -1,15 +1,12 @@
 from ..base import APITest
-import abc
 
 
 URL = '/v1/claims/'
 
 
-class ClaimUpdateMixinBase(object):
-    __metaclass__ = abc.ABCMeta
-
+class ClaimPatchBase(APITest):
     def setUp(self):
-        super(ClaimUpdateMixinBase, self).setUp()
+        super(ClaimPatchBase, self).setUp()
         self.post_data = {
             'resource': 'update-test-resource',
             'ttl': 20,
@@ -18,54 +15,42 @@ class ClaimUpdateMixinBase(object):
         self.post_response = self.post(URL, self.post_data)
         self.resource_url = self.post_response.headers['Location']
 
-    @abc.abstractproperty
-    def method(self):
-        pass
 
-    @abc.abstractmethod
-    def method_data(self, data):
-        pass
-
-    def update(self, url, data):
-        method = getattr(self, self.method)
-        return method(url, data=self.method_data(data))
-
-
-class ClaimUpdateSuccessMixin(ClaimUpdateMixinBase):
+class ClaimPatchSuccess(ClaimPatchBase):
     def test_update_status_from_active_to_active_should_return_200(self):
-        update_response = self.update(self.resource_url, {'status': 'active'})
+        update_response = self.patch(self.resource_url, {'status': 'active'})
         self.assertEqual(200, update_response.status_code)
 
     def test_update_status_from_active_to_released_should_return_204(self):
-        update_response = self.update(self.resource_url, {'status': 'released'})
+        update_response = self.patch(self.resource_url, {'status': 'released'})
         self.assertEqual(204, update_response.status_code)
 
     def test_update_status_from_active_to_released_should_set_status(self):
-        update_response = self.update(self.resource_url, {'status': 'released'})
+        update_response = self.patch(self.resource_url, {'status': 'released'})
         get_response = self.get(self.resource_url)
         self.assertEqual('released', get_response.DATA['status'])
 
     def test_update_status_from_active_to_revoked_should_return_204(self):
-        update_response = self.update(self.resource_url, {'status': 'revoked'})
+        update_response = self.patch(self.resource_url, {'status': 'revoked'})
         self.assertEqual(204, update_response.status_code)
 
     def test_update_status_from_active_to_revoked_should_set_status(self):
-        update_response = self.update(self.resource_url, {'status': 'revoked'})
+        update_response = self.patch(self.resource_url, {'status': 'revoked'})
         get_response = self.get(self.resource_url)
         self.assertEqual('revoked', get_response.DATA['status'])
 
 
     def test_update_status_from_waiting_to_active_should_return_200(self):
         second_post_response = self.post(URL, self.post_data)
-        self.update(self.resource_url, {'status': 'released'})
-        update_response = self.update(second_post_response.headers['Location'],
+        self.patch(self.resource_url, {'status': 'released'})
+        update_response = self.patch(second_post_response.headers['Location'],
                 {'status': 'active'})
         self.assertEqual(200, update_response.status_code)
 
     def test_update_status_from_waiting_to_active_should_set_status(self):
         second_post_response = self.post(URL, self.post_data)
-        self.update(self.resource_url, {'status': 'released'})
-        update_response = self.update(second_post_response.headers['Location'],
+        self.patch(self.resource_url, {'status': 'released'})
+        update_response = self.patch(second_post_response.headers['Location'],
                 {'status': 'active'})
         self.assertEqual('active', update_response.DATA['status'])
 
@@ -80,7 +65,7 @@ class ClaimUpdateSuccessMixin(ClaimUpdateMixinBase):
 
     def test_update_status_from_waiting_to_revoked_should_return_204(self):
         second_post_response = self.post(URL, self.post_data)
-        response = self.update(second_post_response.headers['Location'],
+        response = self.patch(second_post_response.headers['Location'],
                 {'status': 'revoked'})
         self.assertEqual(204, response.status_code)
 
@@ -94,33 +79,33 @@ class ClaimUpdateSuccessMixin(ClaimUpdateMixinBase):
 #        pass
 
 
-class ClaimUpdateErrorMixin(ClaimUpdateMixinBase):
+class ClaimPatchError(ClaimPatchBase):
 # TODO
 #    def test_unknown_parameters_should_return_400(self):
 #        pass
 
     def test_invalid_parameters_should_return_400(self):
-        expired_status_response = self.update(self.resource_url,
+        expired_status_response = self.patch(self.resource_url,
                 {'status': 'expired'})
         self.assertEqual(400, expired_status_response.status_code)
         self.assertIn('status', expired_status_response.data)
 
-        waiting_status_response = self.update(self.resource_url,
+        waiting_status_response = self.patch(self.resource_url,
                 {'status': 'waiting'})
         self.assertEqual(400, waiting_status_response.status_code)
         self.assertIn('status', waiting_status_response.data)
 
-        timeout_response = self.update(self.resource_url,
+        timeout_response = self.patch(self.resource_url,
                 {'ttl': -1.7})
         self.assertEqual(400, timeout_response.status_code)
         self.assertIn('ttl', timeout_response.data)
 
     def test_update_with_no_parameters_should_return_400(self):
-        invalid_response = self.update(self.resource_url, {})
+        invalid_response = self.patch(self.resource_url, {})
         self.assertEqual(400, invalid_response.status_code)
 
     def test_non_existent_claim_should_return_404(self):
-        invalid_response = self.update('/v1/claims/123456789/',
+        invalid_response = self.patch('/v1/claims/123456789/',
                  {'status': 'active'})
         self.assertEqual(404, invalid_response.status_code)
 
@@ -133,22 +118,22 @@ class ClaimUpdateErrorMixin(ClaimUpdateMixinBase):
 #        pass
 
     def test_updating_released_claim_should_return_409(self):
-        self.update(self.resource_url, {'status': 'released'})
+        self.patch(self.resource_url, {'status': 'released'})
         statuses = ['active', 'released', 'revoked']
         for status in statuses:
-            response = self.update(self.resource_url, {'status': status})
+            response = self.patch(self.resource_url, {'status': status})
             self.assertEqual(409, response.status_code)
 
     def test_updating_revoked_claim_should_return_409(self):
-        self.update(self.resource_url, {'status': 'revoked'})
+        self.patch(self.resource_url, {'status': 'revoked'})
         statuses = ['active', 'released', 'revoked']
         for status in statuses:
-            response = self.update(self.resource_url, {'status': status})
+            response = self.patch(self.resource_url, {'status': status})
             self.assertEqual(409, response.status_code)
 
     def test_updating_status_from_waiting_to_released_should_return_409(self):
         second_post_response = self.post(URL, self.post_data)
-        response = self.update(second_post_response.headers['Location'],
+        response = self.patch(second_post_response.headers['Location'],
                 {'status': 'released'})
         self.assertEqual(409, response.status_code)
 
@@ -158,49 +143,12 @@ class ClaimUpdateErrorMixin(ClaimUpdateMixinBase):
 
     def test_updating_status_to_active_with_contention_should_return_409(self):
         second_post_response = self.post(URL, self.post_data)
-        response = self.update(second_post_response.headers['Location'],
+        response = self.patch(second_post_response.headers['Location'],
                 {'status': 'active'})
         self.assertEqual(409, response.status_code)
 
     def test_release_inactive_lock_should_return_409(self):
         second_post_response = self.post(URL, self.post_data)
-        response = self.update(second_post_response.headers['Location'],
+        response = self.patch(second_post_response.headers['Location'],
                 {'status': 'released'})
         self.assertEqual(409, response.status_code)
-
-
-
-class ClaimDetailPatchMixin(object):
-    method = 'patch'
-
-    def method_data(self, data):
-        return data
-
-class ClaimDetailPutMixin(object):
-    method = 'put'
-
-    def method_data(self, data):
-        put_data = {
-            'ttl': 0.010,
-            'status': 'waiting',
-        }
-        put_data.update(data)
-        return put_data
-
-
-class ClaimDetailPatchSuccessTest(ClaimDetailPatchMixin,
-        ClaimUpdateSuccessMixin, APITest):
-    pass
-
-class ClaimDetailPatchErrorTest(ClaimDetailPatchMixin,
-        ClaimUpdateErrorMixin, APITest):
-    pass
-
-
-class ClaimDetailPutSuccessTest(ClaimDetailPutMixin,
-        ClaimUpdateSuccessMixin, APITest):
-    pass
-
-class ClaimDetailPutErrorTest(ClaimDetailPutMixin,
-        ClaimUpdateErrorMixin, APITest):
-    pass

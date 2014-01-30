69869145035e1699f75f735f272f001ed469b2e2
Mark Burnett <mark.m.burnett@gmail.com>
2014-01-30 08:32:29 -0600

    claim release does not activate other claims

--- tests/api/v1/test_contention.py
+++ tests/api/v1/test_contention.py
@@ -29,6 +29,19 @@ class ContentionTest(APITest):
 
         self.assertEqual(200, activate_response.status_code)
 
-# TODO
-#    def test_claim_release_should_not_activate_other_claims(self):
-#        pass
+    def test_claim_release_should_not_activate_other_claims(self):
+        post_data = {
+            'resource': 'everyone-wants-it',
+            'ttl': 600,
+        }
+
+        first_post_response = self.post(URL, post_data)
+        second_post_response = self.post(URL, post_data)
+        third_post_response = self.post(URL, post_data)
+
+        self.patch(first_post_response.headers['Location'],
+                {'status': 'released'})
+        activate_response = self.patch(third_post_response.headers['Location'],
+                {'status': 'active'})
+
+        self.assertEqual(409, activate_response.status_code)

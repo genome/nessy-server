5376e47ea3080d5ce801864084866222c77175d4
Mark Burnett <mark.m.burnett@gmail.com>
2014-01-31 07:22:07 -0600

    benchmark client uses command line arguments

--- bench/go.py
+++ bench/go.py
@@ -1,27 +1,68 @@
+#!/usr/bin/env python
+
 from transitions import *
 from loop import loop
 from state import State
+
+import argparse
 import datetime
 
 
-state = State(resource_names=['a', 'b', 'c', 'd', 'e', 'f', 'g'])
-url = 'http://10.0.34.108/v1/claims/'
-#url = 'http://localhost:5000/v1/claims/'
+def main():
+    args = parse_args()
 
-transitions = [
-    Activate(base_rate=50),
-    Create(url=url, base_rate=100, ttl=1),
-    Heartbeat(base_rate=5, ttl=1),
-    Release(base_rate=2),
-    Revoke(base_rate=1),
-]
+    state = State(resource_names=_resource_names(args.resources))
 
-iterations = 1000
+    transitions = [
+        Activate(base_rate=args.activate_rate),
+        Create(url=args.url, base_rate=args.create_rate, ttl=args.create_ttl),
+        Heartbeat(base_rate=args.heartbeat_rate, ttl=args.heartbeat_ttl),
+        Release(base_rate=args.release_rate),
+        Revoke(base_rate=args.revoke_rate),
+    ]
 
-begin = datetime.datetime.now()
-loop(state, transitions, iterations)
-end = datetime.datetime.now()
+    begin = datetime.datetime.now()
+    loop(state, transitions, args.iterations)
+    end = datetime.datetime.now()
 
-print
-print 'iterations seconds'
-print iterations, (end - begin).total_seconds()
+    print 'iterations seconds'
+    print args.iterations, (end - begin).total_seconds()
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('url', default='http://localhost:5000/v1/claims/',
+            nargs='?', help='URL for POSTing new claims')
+
+    parser.add_argument('-i', '--iterations', type=int, default=1000,
+            help='Number of requests to make per process')
+    parser.add_argument('-p', '--processes', type=int, default=1,
+            help='Number of processes')
+
+    parser.add_argument('-r', '--resources', type=int, default=10,
+            help='Number of resources to lock')
+
+    transition_parameters = parser.add_argument_group('Transition Parameters')
+    transition_parameters.add_argument('--activate-rate', type=float,
+            default=50)
+
+    transition_parameters.add_argument('--create-rate', type=float, default=100)
+    transition_parameters.add_argument('--create-ttl', type=float, default=1)
+
+    transition_parameters.add_argument('--heartbeat-rate', type=float,
+            default=5)
+    transition_parameters.add_argument('--heartbeat-ttl', type=float, default=1)
+
+    transition_parameters.add_argument('--release-rate', type=float, default=2)
+    transition_parameters.add_argument('--revoke-rate', type=float, default=1)
+
+    return parser.parse_args()
+
+
+def _resource_names(num_resources):
+    return ['bench-resource-%d' % i for i in xrange(num_resources)]
+
+
+if __name__ == '__main__':
+    main()

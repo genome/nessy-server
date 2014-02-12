6e241f63e83827c3d28e89c6f74854e8464fbc7b
Mark Burnett <mark.m.burnett@gmail.com>
2014-02-12 11:29:07 -0600

    add statsd timers to view methods

--- nessy/api/application.py
+++ nessy/api/application.py
@@ -1,12 +1,15 @@
 from . import v1
 from .. import backend
 import flask
+import os
+import statsd
 
 
 __all__ = ['create_app']
 
 
 def create_app(database_string, purge=False):
+    _initialize_statsd()
     factory = _create_factory(database_string, purge=purge)
     app = _create_app_from_blueprints()
 
@@ -36,3 +39,8 @@ def _attach_factory_to_app(factory, app):
     @app.teardown_request
     def teardown_request(exception):
         flask.g.actor.cleanup()
+
+def _initialize_statsd():
+    statsd.Connection.set_defaults(
+            host=os.environ.get('LOCKING_STATSD_HOST', 'localhost'),
+            port=os.environ.get('LOCKING_STATSD_PORT', 8125))
--- nessy/api/v1/views.py
+++ nessy/api/v1/views.py
@@ -3,32 +3,38 @@ from .output_fields import claim_fields
 from flask import g, url_for
 from flask.ext.restful import Resource, marshal
 from nessy.backend import exceptions
+from statsd import Timer
 
 
 __all__ = ['ClaimListView', 'ClaimView']
 
 
+timer = Timer('nessy-server')
+
+
 class ClaimListView(Resource):
     def get(self):
-        data, errors = request_parsers.get_claim_list_data()
-        if errors:
-            return errors, 400
+        with timer.time('list-get'):
+            data, errors = request_parsers.get_claim_list_data()
+            if errors:
+                return errors, 400
 
-        result = g.actor.list_claims(**data)
-        return marshal(result, claim_fields)
+            result = g.actor.list_claims(**data)
+            return marshal(result, claim_fields)
 
     def post(self):
-        data, errors = request_parsers.get_claim_post_data()
-        if errors:
-            return errors, 400
+        with timer.time('list-post'):
+            data, errors = request_parsers.get_claim_post_data()
+            if errors:
+                return errors, 400
 
-        claim, ownership = g.actor.create_claim(**data)
-        if ownership:
-            status_code = 201
-        else:
-            status_code = 202
-        return (marshal(claim, claim_fields), status_code,
-                {'Location': _construct_claim_url(claim.id)})
+            claim, ownership = g.actor.create_claim(**data)
+            if ownership:
+                status_code = 201
+            else:
+                status_code = 202
+            return (marshal(claim, claim_fields), status_code,
+                    {'Location': _construct_claim_url(claim.id)})
 
 def _construct_claim_url(claim_id):
     return url_for('claim', id=claim_id)
@@ -36,27 +42,29 @@ def _construct_claim_url(claim_id):
 
 class ClaimView(Resource):
     def get(self, id):
-        claim = g.actor.get_claim(id)
-        if claim:
-            return marshal(claim, claim_fields)
-        else:
-            return {'message': 'No claim found'}, 404
+        with timer.time('detail-get'):
+            claim = g.actor.get_claim(id)
+            if claim:
+                return marshal(claim, claim_fields)
+            else:
+                return {'message': 'No claim found'}, 404
 
     def patch(self, id):
-        data, errors = request_parsers.get_claim_update_data()
-        if errors:
-            return errors, 400
+        with timer.time('detail-patch'):
+            data, errors = request_parsers.get_claim_update_data()
+            if errors:
+                return errors, 400
 
-        try:
-            content = g.actor.update_claim(id, **data)
-            if content is None:
-                return None, 204
-            else:
-                return marshal(content, claim_fields), 200
+            try:
+                content = g.actor.update_claim(id, **data)
+                if content is None:
+                    return None, 204
+                else:
+                    return marshal(content, claim_fields), 200
 
-        except exceptions.InvalidRequest as e:
-            return e.as_dict, 400
-        except exceptions.ClaimNotFound as e:
-            return e.as_dict, 404
-        except exceptions.ConflictException as e:
-            return e.as_dict, 409
+            except exceptions.InvalidRequest as e:
+                return e.as_dict, 400
+            except exceptions.ClaimNotFound as e:
+                return e.as_dict, 404
+            except exceptions.ConflictException as e:
+                return e.as_dict, 409
